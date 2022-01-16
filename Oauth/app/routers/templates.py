b371ae7153201b60c62ea22ac7657f900f347f5d
837477 <8374770@gmail.com>
2022-01-16 21:08:08 +0900

    Update: 코드 설명 주석 수정

--- Oauth/app/routers/templates.py
+++ Oauth/app/routers/templates.py
@@ -14,8 +14,7 @@ templates = Jinja2Templates(directory="app/assets")
 @router.get("/")
 async def index(request: Request):
     """
-    유저 인증 페이지인 Google oauth login URL을 전달.
-    - 인증에 필요한 client_id / redirect_uri 정보를 이미 작성해둔 URL를 jinja를 통하여 전달.
+    사용자 인증 과정의 로그인 페이지인 Oauth login URL을 전달.
     """
     google = GoogleOauth(config)
     kakao = KakaoOauth(config)
